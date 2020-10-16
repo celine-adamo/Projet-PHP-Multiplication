55d7a413dbe002762198e55a6626325c4f378d90
celine-adamo <71389471+celine-adamo@users.noreply.github.com>
2020-10-16 16:08:03 +0200

    CSS / PHP

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item"><a class="page-link" href="#">Précédent</a></li>
+    <li class="page-item"><a class="page-link" href="index.php">1</a></li>
+    <li class="page-item"><a class="page-link" href="page2.php">2</a></li>
+    <li class="page-item"><a class="page-link" href="page3.php">3</a></li>
+    <li class="page-item"><a class="page-link" href="page4.php">4</a></li>
+    <li class="page-item"><a class="page-link" href="page5.php">5</a></li>
+    <li class="page-item"><a class="page-link" href="#">Suivant</a></li>
+  </ul>
+</nav>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Space Multiplication</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/styles/styles.css">
+    <link rel="shortcut icon" href="assets/images/fusee.png" type="image/x-icon">
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="page2.php">Narada</a>
+      <a class="nav-link" href="page3.php">Nostromo</a>
+      <a class="nav-link" href="page4.php">Millenium Falcon</a>
+      <a class="nav-link" href="page5.php">Mondoshawan Vessel</a>
+    </div>
+  </div>
+</nav>
+
